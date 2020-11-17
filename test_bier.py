7f8b3e26b0b7616e9711f831e802f297fced806a
CaptainPerox <gordoncedric@hotmail.com>
2020-11-17 13:57:43 +0100

    initial commit

---
+++ test_bier.py
@@ -0,0 +1,17 @@
+# Test class Bier
+# from model.Bier import Bier
+
+print("** Bier 1 ***")
+# b1 = Bier("Augustijn", "Bios", 12.0, "amber")
+# print(b1)
+# verander de brouwerij naar lege string
+# b1.brouwerij = ""
+# print(b1)
+
+print("** Bier 2 ***")
+# b2 = Bier("Jupiler", "", 3.3, "blond")
+# print(f"Het kleur van _________ is ________ ")
+# print(f"Alcoholpercentage: ___________ ")
+# verander het percentage naar een ongeldige waarde
+# b2.alcoholpercentage = "5,5"
+# print(f"Alcoholpercentage: ___________ ")
---
+++ test_boek.py
@@ -0,0 +1,14 @@
+# Doe een import van het model
+
+
+# maak een nieuw boek aan
+print("\n*******Nieuw boek aanmaken*******")
+
+
+print(f"Titel van boek 1: _______")
+print("Volledige info boek 1")
+
+
+# maak een tweede boek
+print("\n*******Nieuw boek aanmaken*******")
+print("Volledige info boek 2")
